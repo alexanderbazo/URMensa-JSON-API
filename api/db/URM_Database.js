4b57bb7e5c21fa12d621784cbc90d40e78ee82a5
alexanderbazo <fon@pc108140227X.uni-regensburg.de>
2015-03-16 10:36:31 +0100

    added placeholder for vote integration

--- api/db/URM_Database.js
+++ api/db/URM_Database.js
@@ -4,13 +4,28 @@
 var URMDatabase = function (newDownloader) {
     var downloader = newDownloader,
         menu = [],
+        //read from file: ID,name,up,down
+        votes = [],
+
+        initVotes = function () {
+            //load from file
+            //iterate menu
+            //add dish if not exist < create id
+            //get votes and store in item
+        },
 
         update = function () {
             console.log("updating");
             var weekNumber = (new Date()).getWeek();
             downloader.get("www.stwno.de", "/infomax/daten-extern/csv/UNI-R/" + weekNumber + ".csv", function (data) {
                 menu = data;
+                initVotes();
             });
+        },
+
+        save = function () {
+            //save votes to file
+
         },
 
         filterByDay = function (day, element) {
@@ -20,11 +35,17 @@ var URMDatabase = function (newDownloader) {
         getMenuForDay = function (day) {
             var menuForDay = menu.filter(filterByDay.bind(this, day));
             return menuForDay;
-        };
+        },
+
+        upvoteElement = function (id) {},
+
+        downvoteElement = function (id) {};
 
     return {
         update: update,
-        getMenuForDay: getMenuForDay
+        getMenuForDay: getMenuForDay,
+        upvoteElement: upvoteElement,
+        downvoteElement: downvoteElement
     };
 };
 
