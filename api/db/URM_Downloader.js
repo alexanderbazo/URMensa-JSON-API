67531f6722c2f5bfdc5d5a1dd72cdd7735cdbfb0
Alexander Bazo <alexanderbazo@googlemail.com>
2019-09-04 14:14:06 +0200

    add content information while processing server data

--- api/db/URM_Downloader.js
+++ api/db/URM_Downloader.js
@@ -32,6 +32,7 @@ var URMDownloader = function() {
           item.renameProperty("tag", "day");
           item.renameProperty("warengruppe", "category");
           item.renameProperty("kennz", "labels");
+          item.contentInformation = getContentInformationFromTitle(item.name);
           if (item.name.indexOf("(") !== -1) {
             item.name = item.name.substring(0, item.name.indexOf(
               "(")).trim();
@@ -50,6 +51,41 @@ var URMDownloader = function() {
       }
       callback(items);
     });
+  }
+
+  function getContentInformationFromTitle(title) {
+    let contentInformation = [],
+      regex = new RegExp(/\((.*?)\)/g),
+      results = title.match(regex);
+    if (results === null) {
+      return contentInformation;
+    }
+    for (let i = 0; i < results.length; i++) {
+      let currentGroup = results[i].substring(1, results[i].length - 1).split(
+        ",");
+      for (let j = 0; j < currentGroup.length; j++) {
+        if (contentInformation.includes(currentGroup[j]) === false) {
+          contentInformation.push(currentGroup[j]);
+        }
+      }
+    }
+    return translateContentInformation(contentInformation);
+  }
+
+  function translateContentInformation(contentInformation) {
+    for (let i = 0; i < contentInformation.length; i++) {
+      contentInformation[i] = contentInformation[i] === "1" ? "mit Farbstoff" :
+        contentInformation[i];
+      contentInformation[i] = contentInformation[i] === "2" ?
+        "mit Konservierungsstoff" : contentInformation[i];
+      contentInformation[i] = contentInformation[i] === "3" ?
+        "mit Antioxidationsmittel" : contentInformation[i];
+      contentInformation[i] = contentInformation[i] === "4" ?
+        "mit Geschmacksverstärker" : contentInformation[i];
+      contentInformation[i] = contentInformation[i] === "5" ? "geschwefelt " :
+        contentInformation[i];
+    }
+    return contentInformation;
   }
 
   function get(host, path, callback) {
